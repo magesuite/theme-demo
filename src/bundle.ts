3e6cdc6cc646e6c48f16a5e1a8395e100643aa8b
Milena Karpowska <milena.karpowska@creativestyle.pl>
2018-03-27 08:53:47 +0200

    Footer + colors' refactoring

--- src/bundle.ts
+++ src/bundle.ts
@@ -17,12 +17,16 @@ import '../../theme-creativeshop/src/components/collapse/collapse';
 import AddressAutofill from './customizations/address-autofill/address-autofill';
 export { AddressAutofill };
 
-import '../../theme-creativeshop/src/customizations/offcanvas-navigation/offcanvas-navigation';
-import '../../theme-creativeshop/src/customizations/offcanvas/offcanvas';
-import '../../theme-creativeshop/src/customizations/brand-carousel/brand-carousel';
+import { init as collapsibleText } from '../../theme-creativeshop/src/components/collapsible-text/collapsible-text';
+collapsibleText();
 
-import '../../theme-creativeshop/src/customizations/grid-layout/grid-layout';
+import '../../theme-creativeshop/src/customizations/aftersearch-nav/aftersearch-nav';
+import '../../theme-creativeshop/src/customizations/brand-carousel/brand-carousel';
+import '../../theme-creativeshop/src/customizations/header-search/header-search';
 import '../../theme-creativeshop/src/customizations/hero/hero';
 import '../../theme-creativeshop/src/customizations/item-cloner/item-cloner';
+import '../../theme-creativeshop/src/customizations/offcanvas-navigation/offcanvas-navigation';
+import '../../theme-creativeshop/src/customizations/offcanvas/offcanvas';
+import '../../theme-creativeshop/src/customizations/grid-layout/grid-layout';
 import '../../theme-creativeshop/src/customizations/navigation/navigation';
 import '../../theme-creativeshop/src/customizations/products-promo/products-promo';
