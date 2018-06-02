e457c9b41457c0f5ff4f87c8ad801d6a17cc8b9a
Milena Karpowska <milena.karpowska@creativestyle.pl>
2018-06-02 10:52:17 +0200

    Add feature - button 'ok' on search input is displayed all time

---
+++ src/requirejs-config.js
@@ -0,0 +1,13 @@
+var config = {
+    config: {
+        mixins: {
+            'Smile_ElasticsuiteCore/js/form-mini': {
+                'Smile_ElasticsuiteCore/js/form-mini-ext-brh': true,
+            },
+
+            'Magento_Search/form-mini': {
+                'Magento_Search/form-mini-ext-search': true,
+            },
+        },
+    },
+};
