7fff211e534d894c26a71f97a31207e0ab9e7956
Piotr Tabor <piotrtabor@Piotrs-MacBook-Pro.local>
2018-12-21 20:06:35 +0100

    initial commit

--- src/bundle.ts
+++ src/bundle.ts
@@ -12,38 +12,34 @@
 import 'vendors';
 import $ from 'jquery';
 
-import '../../theme-creativeshop/src/components/collapse/collapse';
-import { Flyout } from '../../theme-creativeshop/src/components/flyout/class.flyout';
-export { Flyout };
-
 import AddressAutofill from './customizations/address-autofill/address-autofill';
 export { AddressAutofill };
-
+import { Flyout } from '../../theme-creativeshop/src/components/flyout/class.flyout';
+export { Flyout };
 import { init as collapsibleText } from '../../theme-creativeshop/src/components/collapsible-text/collapsible-text';
 collapsibleText();
-
 import { QtyIncrementCollection } from '../../theme-creativeshop/src/customizations/qty-increment/qty-increment';
 new QtyIncrementCollection();
-
 import Select from '../../theme-creativeshop/src/customizations/select/select';
 export { Select };
 
 import './customizations/aftersearch-nav/aftersearch-nav-filter';
 
+import '../../theme-creativeshop/src/components/collapse/collapse';
 import '../../theme-creativeshop/src/customizations/aftersearch-nav/aftersearch-nav';
 import '../../theme-creativeshop/src/customizations/brand-carousel/brand-carousel';
+import '../../theme-creativeshop/src/customizations/dailydeal/dailydeal';
+import '../../theme-creativeshop/src/customizations/grid-layout/grid-layout';
 import '../../theme-creativeshop/src/customizations/header-search/header-search';
 import '../../theme-creativeshop/src/customizations/hero/hero';
-import '../../theme-creativeshop/src/customizations/item-cloner/item-cloner';
-import '../../theme-creativeshop/src/customizations/offcanvas-navigation/offcanvas-navigation';
-import '../../theme-creativeshop/src/customizations/offcanvas/offcanvas';
-import '../../theme-creativeshop/src/customizations/grid-layout/grid-layout';
-import '../../theme-creativeshop/src/customizations/navigation/navigation';
-import '../../theme-creativeshop/src/customizations/products-promo/products-promo';
 import '../../theme-creativeshop/src/customizations/image-teaser/image-teaser';
 import '../../theme-creativeshop/src/customizations/indicators/fast-shipping-init';
-import '../../theme-creativeshop/src/customizations/dailydeal/dailydeal';
+import '../../theme-creativeshop/src/customizations/item-cloner/item-cloner';
+import '../../theme-creativeshop/src/customizations/navigation/navigation';
+import '../../theme-creativeshop/src/customizations/offcanvas-navigation/offcanvas-navigation';
+import '../../theme-creativeshop/src/customizations/offcanvas/offcanvas';
 import '../../theme-creativeshop/src/customizations/plugincompany-contactforms/plugincompany-contactforms';
-import '../../theme-creativeshop/src/customizations/video-player/video-player';
-import '../../theme-creativeshop/src/customizations/searchresults-switcher/searchresults-switcher';
 import '../../theme-creativeshop/src/customizations/product-finder/product-finder';
+import '../../theme-creativeshop/src/customizations/products-promo/products-promo';
+import '../../theme-creativeshop/src/customizations/searchresults-switcher/searchresults-switcher';
+import '../../theme-creativeshop/src/customizations/video-player/video-player';
