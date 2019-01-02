fda9bf1c1c1d06d32f47db9c3fb87431a662748c
Piotr Tabor <piotrtabor@Piotrs-MacBook-Pro.local>
2019-01-02 14:34:10 +0100

    master branch fixup

--- src/registration.php
+++ src/registration.php
@@ -5,6 +5,6 @@
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Creativestyle/theme-brh',
+    'frontend/Creativestyle/theme-demo',
     __DIR__
 );
